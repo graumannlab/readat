e407181dbd273074fcde8c6d37ada9f77e638ce8
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-09-09 16:17:45 +0300

    added getChromosomalPositions(), updated roxygen docs

--- R/getEnsemblIds.R
+++ R/getEnsemblIds.R
@@ -1,19 +1,24 @@
 #' Get Ensembl IDs by SeqID
 #'
-#' Gets the Ensembl IDs associated with SomaLogic SeIDs.
-#' @param seqIds A character vector of SomaLogic SeqIDs, or \code{NULL} to use
-#' all 1129 SeqIDs.
+#' Gets the Ensembl IDs associated with SomaLogic Sequence IDs.
+#' @param seqIds A character vector of SomaLogic Sequence IDs, or \code{NULL} to use
+#' all 1129 Sequence IDs.
+#' @param simplify Logical.  Should the output be collapsed into a single
+#' data.frame?
 #' @return A list of character vectors.  The names of the list are the input
-#' SeqIDs, and the character vector associated with that element contains the
+#' Sequence IDs, and the character vector associated with that element contains the
 #' Ensembl IDs.
 #' @examples
 #' # Each SeqID may have one, many, or zero associated Ensembl IDs
 #' getEnsemblIds(c("2278-61_4", "4703-87_2", "4916-2_1"))
 #'
 #' # Get everything in the 1129 panel.
+#' \dontrun{
 #' getEnsemblIds()
+#' }
+#' @importFrom listless list_to_data.frame
 #' @export
-getEnsemblIds <- function(seqIds = NULL)
+getEnsemblIds <- function(seqIds = NULL, simplify = FALSE)
 {
   e <- new.env()
   if(is.null(seqIds))
@@ -22,14 +27,23 @@ getEnsemblIds <- function(seqIds = NULL)
     seqIds = e$ids$SeqId
   }
   data(list = "ensembl1129", package = "somalogic", envir = e)
-  e$ensemblIds[names(e$ensemblIds) %in% seqIds]
+  ensemblIds <- e$ensemblIds[names(e$ensemblIds) %in% seqIds]
+  if(simplify)
+  {
+    list_to_data.frame(ensemblIds, "SeqId", "EnsemblId")
+  } else
+  {
+    ensemblIds
+  }
 }
 
 #' Get UniProt Keywords by SeqID
 #'
-#' Gets the UniProt Keywords associated with SomaLogic SeIDs.
+#' Gets the UniProt Keywords associated with SomaLogic Sequence IDs.
 #' @param seqIds A character vector of SomaLogic Sequence IDs, or \code{NULL} to
 #' use all 1129 Sequence IDs.
+#' @param simplify Logical.  Should the output be collapsed into a single
+#' data.frame?
 #' @return A list of data frames.  The names of the list are the input
 #' SeqIds, and the data frame associated with that element contains:
 #' \describe{
@@ -40,12 +54,15 @@ getEnsemblIds <- function(seqIds = NULL)
 #' }
 #' @examples
 #' # Each SeqID may have one, many, or zero associated Ensembl IDs
-#' getEnsemblIds(c("2278-61_4", "4703-87_2", "4916-2_1"))
+#' getUniProtKeywords(c("2278-61_4", "4703-87_2", "4916-2_1"))
 #'
 #' # Get everything in the 1129 panel.
-#' getEnsemblIds()
+#' \dontrun{
+#' getUniProtKeywords()
+#' }
+#' @importFrom dplyr bind_rows
 #' @export
-getUniProtKeywords <- function(seqIds = NULL)
+getUniProtKeywords <- function(seqIds = NULL, simplify = FALSE)
 {
   e <- new.env()
   if(is.null(seqIds))
@@ -54,8 +71,58 @@ getUniProtKeywords <- function(seqIds = NULL)
     seqIds = e$ids$SeqId
   }
   data(list = "uniprotKeywords1129", package = "somalogic", envir = e)
-  e$uniprotKeywords[names(e$uniprotKeywords) %in% seqIds]
+  uniprotKeywords <- e$uniprotKeywords[names(e$uniprotKeywords) %in% seqIds]
+  if(simplify)
+  {
+    bind_rows(uniprotKeywords, .id = "SeqId")
+  } else
+  {
+    uniprotKeywords
+  }
 }
 
+#' Get Chromosomal Positions by SeqID
+#'
+#' Gets the chromosomal positions associated with SomaLogic Sequence IDs.
+#' @param seqIds A character vector of SomaLogic Sequence IDs, or \code{NULL} to
+#' use all 1129 Sequence IDs.
+#' @param simplify Logical.  Should the output be collapsed into a single
+#' data.frame?
+#' @return A list of data frames.  The names of the list are the input
+#' SeqIds, and the data frame associated with that element contains:
+#' \describe{
+#' \item{UniProtId}{Character. The UniProt ID that the Keyword is associated
+#' with.}
+#' \item{Keyword}{Character. A UniProt Keyword associated with the SeqID and
+#' UniProt ID.}
+#' }
+#' @examples
+#' # Each SeqID may have one, many, or zero associated chromosomal positions
+#' getChromosomalPositions(c("2278-61_4", "4703-87_2", "4916-2_1"))
+#'
+#' # Get everything in the 1129 panel.
+#' \dontrun{
+#' getChromosomalPositions()
+#' }
+#' @importFrom dplyr bind_rows
+#' @export
+getChromosomalPositions <- function(seqIds = NULL, simplify = FALSE)
+{
+  e <- new.env()
+  if(is.null(seqIds))
+  {
+    data(list = "ids1129", package = "somalogic", envir = e)
+    seqIds = e$ids$SeqId
+  }
+  data(list = "chromosome1129", package = "somalogic", envir = e)
+  chromosomalPositions <- e$chromosomalPositions[names(e$chromosomalPositions) %in% seqIds]
+  if(simplify)
+  {
+    bind_rows(chromosomalPositions, .id = "SeqId")
+  } else
+  {
+    chromosomalPositions
+  }
+}
 
 
