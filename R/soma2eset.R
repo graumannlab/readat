729aa066836e3bcbc7c44734aef8e2e7b20e945a
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-09-09 12:21:40 +0300

    added missing imports

--- R/soma2eset.R
+++ R/soma2eset.R
@@ -1,5 +1,5 @@
 # Project: somalogic
-# 
+#
 # Author: Aditya Bhagwat
 ###############################################################################
 
@@ -9,34 +9,37 @@
 #' @param log2Transform whether to log2 transform intensities or not
 #' @return ExpressionSet object
 #' @author Aditya Bhagwat
-#' @import Biobase
+#' @importFrom Biobase ExpressionSet
+#' @importFrom Biobase AnnotatedDataFrame
+#' @importFrom Biobase exprs
+#' @importFrom Biobase exprs<-
 #' @export
 soma2eset <- function(somaObj, log2Transform = TRUE){
-   
+
    # assayData
    myIntensities <- t(getIntensities(somaObj))
    myMeta <- getMetadata(somaObj)
-   
+
    # fData
    featureDF <- getSequenceInfo(somaObj)
    featureDF <- data.frame(featureDF)
    rownames(featureDF) <- featureDF$SeqId
-   
+
    # pData
    sampleDF <- data.frame(
-         soma_sample_id = somaObj$SampleId, 
+         soma_sample_id = somaObj$SampleId,
          soma_plate_id = somaObj$PlateId,
          soma_ext_id = somaObj$ExtIdentifier,
          row.names = somaObj$SampleId)
-   
+
    # forge eset
    myEset <- ExpressionSet(myIntensities, AnnotatedDataFrame(sampleDF), AnnotatedDataFrame(featureDF))
-   
+
    # log2 transform
    if (log2Transform){
       exprs(myEset) <- log2(exprs(myEset))
    }
-   
+
    # return eset
    return(myEset)
 }
