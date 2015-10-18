a7a062280989767525633a382b385c108743d512
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-10-18 13:34:06 +0300

    all sample data included in pData element

--- R/soma2eset.R
+++ R/soma2eset.R
@@ -16,30 +16,30 @@
 #' @export
 soma2eset <- function(somaObj, log2Transform = TRUE){
 
-   # assayData
-   myIntensities <- t(getIntensities(somaObj))
-   myMeta <- getMetadata(somaObj)
+  # assayData
+  myIntensities <- t(getIntensities(somaObj))
+  myMeta <- getMetadata(somaObj)
 
-   # fData
-   featureDF <- getSequenceData(somaObj)
-   featureDF <- data.frame(featureDF)
-   rownames(featureDF) <- featureDF$SeqId
+  # fData
+  featureDF <- getSequenceData(somaObj)
+  featureDF <- data.frame(featureDF)
+  rownames(featureDF) <- featureDF$SeqId
 
-   # pData
-   sampleDF <- data.frame(
-         soma_sample_id = somaObj$SampleId,
-         soma_plate_id = somaObj$PlateId,
-         soma_ext_id = somaObj$ExtIdentifier,
-         row.names = somaObj$SampleId)
+  # pData
+  sampleDF <- getSampleData(somaObj)
 
-   # forge eset
-   myEset <- ExpressionSet(myIntensities, AnnotatedDataFrame(sampleDF), AnnotatedDataFrame(featureDF))
+  # forge eset
+  myEset <- ExpressionSet(
+    myIntensities,
+    AnnotatedDataFrame(sampleDF),
+    AnnotatedDataFrame(featureDF)
+  )
 
-   # log2 transform
-   if (log2Transform){
-      exprs(myEset) <- log2(exprs(myEset))
-   }
+  # log2 transform
+  if (log2Transform){
+    exprs(myEset) <- log2(exprs(myEset))
+  }
 
-   # return eset
-   return(myEset)
+  # return eset
+  return(myEset)
 }
