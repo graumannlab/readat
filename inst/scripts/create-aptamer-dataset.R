8cf54d4b30ab3890b18e7080b4c88530cca25263
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-11-01 10:45:01 +0300

    added aptamer description dataset + script to create it

---
+++ inst/scripts/create-aptamer-dataset.R
@@ -0,0 +1,31 @@
+library(data.table)
+library(dplyr)
+library(stringi)
+
+aptamers <- fread(file.choose(), skip = 2)
+
+# Fix repeated colnames
+colnames(aptamers) <- colnames(aptamers) %>%
+  make.names(unique = TRUE)
+
+# Only get useful bits
+aptamers %<>%
+  select_(
+    ~ SeqId,
+    AptamerId = ~ aptamer, # SeqId, truncated at underscore
+    ~ SomaId,
+    ~ Target,
+    ~ TargetFullName,
+    ~ UniProt,
+    IsIn1129Panel = ~ X1.1k,
+    IsIn1310Panel = ~ X1.3k,
+    PlasmaDilution = ~ Plasma,
+    SerumDilution = ~ Serum
+  ) %>%
+  mutate_(
+    UniProt = ~ stri_replace_first_regex(UniProt, "[, ]+", " "),
+    IsIn1129Panel = ~ as.logical(IsIn1129Panel),
+    IsIn1310Panel = ~ as.logical(IsIn1310Panel)
+  )
+
+save(aptamers, file = "data/aptamers.rda")
