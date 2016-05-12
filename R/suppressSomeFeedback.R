f2cc6cbe1878fc6e8fa72f6c6f9ec7c793c7172a
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2016-05-12 12:36:10 +0300

    use exported testthat functionality rather than duplicating

--- R/suppressSomeFeedback.R
+++ R/suppressSomeFeedback.R
@@ -14,12 +14,13 @@
 #' suppressSomeFeedback(log(-1), warnRegex = "NaN")
 #' }
 #' @importFrom stringi stri_detect_regex
+#' @importFrom testthat evaluate_promise
 #' @noRd
 suppressSomeFeedback <- function(expr, msgRegex = NULL, warnRegex = NULL)
 {
   # Could use pander::evals or set options(warn = 1) + capture.output. See
   # https://stat.ethz.ch/pipermail/r-devel/2015-November/072046.html
-  evaluated <- evaluate_promise(expr)
+  evaluated <- testthat::evaluate_promise(expr)
   if(!is.null(msgRegex))
   {
     evaluated$messages <- evaluated$messages[
@@ -35,40 +36,5 @@ suppressSomeFeedback <- function(expr, msgRegex = NULL, warnRegex = NULL)
   }
   lapply(evaluated$warnings, warning, call. = FALSE)
   evaluated$result
-}
-
-
-# Borrowed from testthat
-evaluate_promise <- function (code, print = FALSE)
-{
-    warnings <- character()
-    wHandler <- function(w) {
-        warnings <<- c(warnings, w$message)
-        invokeRestart("muffleWarning")
-    }
-    messages <- character()
-    mHandler <- function(m) {
-        messages <<- c(messages, m$message)
-        invokeRestart("muffleMessage")
-    }
-    temp <- file()
-    on.exit(close(temp))
-    result <- with_sink(temp, withCallingHandlers(withVisible(code),
-        warning = wHandler, message = mHandler))
-    if (result$visible && print) {
-        with_sink(temp, print(result$value))
-    }
-    output <- paste0(readLines(temp, warn = FALSE), collapse = "\n")
-    list(result = result$value, output = output, warnings = warnings,
-        messages = messages)
-}
-
-
-# Also from testthat
-with_sink <- function (connection, code, ...)
-{
-    sink(connection, ...)
-    on.exit(sink())
-    code
 }
 
