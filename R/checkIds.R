8f47fb6bb349b78eb0ea7ae648e5d04aab47156d
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2016-05-18 11:50:29 +0300

    updated gene symbol regex + added tests

--- R/checkIds.R
+++ R/checkIds.R
@@ -1,10 +1,28 @@
 # See http://www.uniprot.org/help/accession_numbers
-UNIPROT_ID <- "(?:[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9](?:[A-Z][A-Z0-9]{2}[0-9]){1,2})"
+UNIPROT_ID <- "^(?:[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9](?:[A-Z][A-Z0-9]{2}[0-9]){1,2})$"
 
 ENTREZ_GENE_ID <- "^(?:[0-9]+)$"
 
-ENTREZ_GENE_SYMBOL <- "^(?:(?:OK/|b|DKFZp)?(?:[A-Z0-9.]+)(?:orf[A-Z0-9]+)?(?:-[A-Z0-9.]+)?)(?:orf[A-Z0-9]+)?(?:_[2HB]|@|-[A-Z0-9]+)?$"
+# From http://www.genenames.org/about/guidelines#genesymbols
+## 1. The initial character of the symbol should always be a letter.
+## 2. Subsequent characters may be other letters, or if necessary, Arabic numerals.
+## 3. No punctuation may be used, with the exception of hyphens in specific families and locus types
 
+# From http://www.genenames.org/about/guidelines#specificusage
+# @, # punctuation character allowed (all other allowed prefixes/suffixes are
+# letters)
+
+# From http://www.genenames.org/about/guidelines#Appendix1
+# When referring to splice variants, the symbol can be followed by an
+# underscore and the lower case letter "v" then a consecutive number
+# This last rule is fudged a little to allow arbitrary letters and numbers after
+# the underscore.  There are quite a few cases of hyphen then number in the
+# EBI HGNC HS dataset
+# ftp://ftp.ebi.ac.uk/pub/databases/genenames/new/tsv/hgnc_complete_set.txt
+
+# In the broader scheme of things, Entrez Gene Symbols have no common pattern.
+# ftp://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/All_Data.gene_info.gz
+ENTREZ_GENE_SYMBOL <- "^(?:[A-Z][A-Z0-9]*)(?:[_-][A-Z0-9]+)*[#@]?$"
 
 # Predicates
 #' @importFrom assertive.base call_and_name
@@ -42,7 +60,11 @@ is_entrez_gene_symbol <- function(x)
   call_and_name(
     function(x)
     {
-      ok <- stri_detect_regex(x, paste0(ENTREZ_GENE_SYMBOL, "|^Human-virus$"))
+      ok <- stri_detect_regex(
+        x,
+        paste0(ENTREZ_GENE_SYMBOL, "|^Human-virus$"),
+        case_insensitive = TRUE
+      )
       set_cause(ok, "bad format")
     },
     x
